5e01b74f90e52cbc6c42c6f9e01338d1f2c63460
netonblpb <skullbog@gmail.com>
2022-02-04 09:51:28 +0300

    Task9 complete

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// Task9: Показать последнюю цифру трёхзначного числа
+
+Console.Write("Введите трехзначное число: ");
+string number = Console.ReadLine();
+
+ Console.WriteLine("Третья цифра: " + number[2]);
